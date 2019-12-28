7375a20901673b727f033ba1329337151515246a
John Todd <jqt3of5@gmail.com>
2019-12-27 23:34:00 -0700

    Trying to figure out the binary format

--- Certs/GenerateCertificateAuthority.sh
+++ Certs/GenerateCertificateAuthority.sh
@@ -6,4 +6,6 @@ touch CA/certsdb
 mkdir Device
 
 openssl req -config DeviceCA.conf -new -out CA/device.ca.csr.pem
-openssl ca -config DeviceCA.conf -create_serial -selfsign -extensions ca_extensions -in CA/device.ca.csr.pem
+openssl ca -config DeviceCA.conf -create_serial -selfsign -extensions ca_extensions -in CA/device.ca.csr.pem -out CA/device.ca.pem
+#openssl x509 -in CA/device.ca.pem -outform DER -out CA/device.ca.crt
+#openssl x509 -in CA/private/device.ca.key.pem -outform DER -out CA/private/device.ca.key.der
--- Certs/GenerateDeviceCertificate.sh
+++ Certs/GenerateDeviceCertificate.sh
@@ -1,2 +1,4 @@
 openssl req -config DeviceCertificate.conf -new -out Device/device.csr.pem
 openssl ca -config DeviceCertificate.conf -in Device/device.csr.pem
+#openssl x509 -in Device/device.cert.pem -outform DER -out Device/device.cert.crt
+#openssl x509 -in Device/private/device.key.pem -outform DER -out Device/private/device.key.der
