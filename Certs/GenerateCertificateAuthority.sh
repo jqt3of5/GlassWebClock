b3262bead454b9ceddc308a124902f6a4d3e9e51
John Todd <jqt3of5@gmail.com>
2019-12-28 12:21:07 -0700

    Generate the Root CA for aws iot in a better way

--- Certs/GenerateCertificateAuthority.sh
+++ Certs/GenerateCertificateAuthority.sh
@@ -1,3 +1,4 @@
+
 mkdir CA
 mkdir CA/private
 touch CA/certsdb
@@ -8,8 +9,14 @@ openssl req -config DeviceCA.conf -new -out CA/device.ca.csr.pem
 openssl ca -config DeviceCA.conf -create_serial -selfsign -extensions ca_extensions -in CA/device.ca.csr.pem -out CA/device.ca.pem
 
 openssl genrsa -out CA/private/verificationCert.key 2048
-openssl req -new -key CA/private/verificationCert.key -out CA/verificationCert.csr
-openssl x509 -req -in CA/verificationCert.csr -CA CA/device.ca.pem -CAkey CA/private/device.ca.key.pem -CAcreateserial -out CA/verificationCert.crt -days 500 -sha256
+
+aws2 iot get-registration-code
+
+openssl req -config CAVerification.conf -new -out CA/verificationCert.csr
+openssl ca -config CAVerification.conf -in CA/verificationCert.csr -out CA/verificationCert.pem
+
+aws2 iot register-ca-certificate --set-as-active --allow-auto-registration --ca-certificate file://CA/device.ca.pem --verification-certificate file://CA/verificationCert.pem
+
 
 #openssl x509 -in CA/device.ca.pem -outform DER -out CA/device.ca.crt
 #openssl x509 -in CA/private/device.ca.key.pem -outform DER -out CA/private/device.ca.key.der
